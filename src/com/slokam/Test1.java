927e0ff44061c9df584411870b94afea732c0260
Ram-Root <sairamkrishna350@gmail.com>
2023-07-09 13:36:36 +0530

    second commit

--- src/com/slokam/Test1.java
+++ src/com/slokam/Test1.java
@@ -4,6 +4,85 @@ public class Test1 {
 	
 	public static void main(String[] args) {
 		 System.out.println("Git test .");
+		 
+		 
+		/* ===============================================
+		 sodexo :-
+		 mail : KANDIKONDA.KRISHNAREDDY@LNTINFOTECH.COM
+		 card Ref No : 121356784273
+		 password : Ramkrishna@003
+		 Phone : 7702050018
+		 =================================================
+		 UAN No : 101769433732
+		 Password : Ramkrishna@0003
+		 ===============================================
+		 Medi Buddy Password : Ramkrishna@003
+		 ===================================================
+		 LTI Mail : Kandikonda.KrishnaReddy@lntinfotech.com
+		 Mail Password : Ramkrishn@0003
+		 LTI Employee id or P.S No : 10698459
+		 Microsoft Login : 8367632025
+		 Password : Ramkrishna0003
+		 Outlook pin : 836763
+		 address : THE SKYVIEW BUILDING NO 20 14TH 15TH FLOOR PARTY
+		           SEZ DEVELOPER IN SURVEY NO 83 1 OF RAIDURGAM
+		           SERLINGAMPALLY MANDAL RANGAREDDY HYDERABAD
+		           500081 HYDERABAD.
+
+		 =================================================
+		 ICICI Bank:-
+		 Account No : 030301001919
+		 IFSC : icici0000303
+		 login : 583059302
+		 Password : R@mkrishna0008
+		 phonepy icici bank upi pin - 0967
+		 ====================================================
+		 imobile:-
+		 app lock : 9269
+		 Debit card pin : 6763
+		 =====================================================
+		 Andhra Bank:-
+		 Andhra bank Debit card pin - 9390
+		 phonepy Andhra bank upi pin - 000967
+		 ====================================================
+		 SBI Bank:-
+		 Account No : 34429368828
+		 IFSC : SBIN0010782
+		 user : Ramk/Krishna.Reddy350
+		 Password : R@mkrishna0003
+		 Profile password : r@mKrishna0003
+		 =====================================================
+		 HDFC Bank:-
+		 Account No : 50100498734161
+		 IFSC : HDFC0009605
+		 KYC Registration number : 40005185091288
+		 Customer ID : 187735422
+		 Net Banking Password : R@mkrishna003
+		 phonepy HDFC bank upi pin - 0967
+		 HDFC Debit card pin - 9390
+		 HDFC Credit card Pin - 9390
+		 =====================================================
+		 HSBC Mail : kandikonda.krishnareddy@noexternalmail.hsbc.com
+		 staff ID / peoplesoft ID : 45244804
+		 cTool ID : 316316
+		 Entity Manager : Jyoti N Phalke
+		 HSBC wnidows username : 45244804
+		 Password : R@mkrishna0008
+		 RSA PIN : 836763
+		 ======================================================
+		 Axis Bank Credit card : 9390
+		 Primary Card Activation code : 542903
+		 card No : 4400 0601 1772 9170 
+		 Expiry Date : 07/27
+		 =====================================================================================
+		 practice ecllipse file path : C:\Users\DELL\eclipse\jee-2020-062\eclipse\practice
+		 =====================================================================================
+		 Git Hub details  :
+		 User Name : Ram-Root
+		 mail : sairamkrishna350@gmail.com
+		 password : ramkrishna0003                           */
+
+
 	}
 
 }
